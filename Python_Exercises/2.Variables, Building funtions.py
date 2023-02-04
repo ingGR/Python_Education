38c9392ca5de0410648f56a852fbea77fc77ad90
ingGR <ingeniero.guillermorodriguez@gmail.com>
2023-02-03 19:18:42 -0500

    create 1 and 2 archive

---
+++ Python_Exercises/2.Variables, Building funtions.py
@@ -0,0 +1,24 @@
+# Variables 
+my_str_var = "Hello World"
+my_int_var = 16
+my_bool_var = True
+
+#Concatenation of variables in a funtion print
+print(my_str_var,my_int_var,my_bool_var)
+print("This is a string:"+my_str_var,"This is a int:"+str(my_int_var),"This is a boolean:"+str(my_bool_var))
+
+# some system functions
+print(len(my_str_var))
+
+#Variables in one line
+name, surname, alias, age = "Guillermo", "Rodriguez", "Gram", 31
+print("I'm:",age,"old","My surname is: ", surname,"My name is:", name, "And my nickname is:",alias)
+
+#Function "Input"
+first_name = input("What is your name: ")
+age = input("How old are you? ")
+
+print(first_name)
+print(age)
+
+
